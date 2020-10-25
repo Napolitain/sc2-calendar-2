73069741dae3c4f5d5b3710020ad22fb4032caa3
Maxime "Napolitain" Boucher <mxboucher@gmail.com>
2020-10-25 18:29:54 +0100

    static class added and principles written (requests)

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,87 @@
-var express = require('express');
-var https = require("https");
-var router = express.Router();
+const express = require("express");
+const axios = require("axios").default;
+const router = express.Router();
+const cheerio = require("cheerio");
+const ical = require("node-ical");
 
 /* GET home page. */
 router.get('/', function(req, response, next) {
   response.render('index', { title: 'Express' });
+  App.setPlayers(req.query.players);
+  //App.getLiquipediaMatches()
 });
 
 /**
- * Function that requests Liquipedia upcoming matches
+ * Static class which is the whole application.
  */
-function getLiquipediaMatches() {
-  let options = {
-    hostname: "liquipedia.net",
-    port: 443,
-    path: "/starcraft2/api.php?action=parse&format=json&page=Liquipedia:Upcoming_and_ongoing_matches",
-    method: "GET"
-  };
-  let request = https.request(options, response => {
-    response.on("data", data => {
-      return data;
-    });
-  });
-  request.on("error", error => {
-    console.error("Error while requesting data to Liquipedia: ", error);
-  });
+class App {
+
+    /**
+     * HTML page
+     * @type {string}
+     */
+    static data = "";
+
+    /**
+     * Players list (from querystring)
+     * @type {[]}
+     */
+    static players = [];
+
+    /**
+     * If data expired from Redis, calls App.getLiquipediaMatches,
+     * otherwise return App.data
+     * @returns {string}
+     */
+    static getData() {
+        return App.data;
+    }
+
+    /**
+     * Setter for App.data and for Redis
+     * @param d
+     */
+    static setData(d) {
+        App.data = d;
+    }
+
+    /**
+     * Setter for App.players from Express querystring
+     * @param p
+     */
+    static setPlayers(p) {
+        App.players = [];
+        for (const player of p) {
+            App.players.push(player);
+        }
+    }
+
+    /**
+     * Function that requests Liquipedia upcoming matches,
+     * if it doesn't work, log error
+     */
+    static getLiquipediaMatches() {
+        axios.get("https://liquipedia.net/starcraft2/Liquipedia:Upcoming_and_ongoing_matches", {
+            headers: {
+                "Accept-Encoding": "gzip",
+                "User-Agent": "User-Agent\": \"Sc2Calendar/developer (mxboucher@gmail.com)"
+            },
+        })
+            .then(function (response) {
+                App.setData(response.data);
+            })
+            .catch(function (error) {
+                console.error("Couldn't request to Liquipedia: ", error);
+            })
+    }
+
+    static scrapeData() {
+        let matches = App.get
+    }
+
+    static generateEvent() {
+
+    }
 }
 
 module.exports = router;
